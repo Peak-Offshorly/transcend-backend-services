e2267d31936ad2a77390a8faded5118454609244
Karlos <karlos@offshorly.com>
2024-04-05 14:33:32 +0800

    add: connection to postgresqlDB, initial Traits and TraitsQuestions models

--- app/database/models.py
+++ app/database/models.py
@@ -1,1 +1,20 @@
-from sqlalchemy import Boolean,Column,ForeignKey,Integer,String
+import uuid
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+from app.database.connection import Base, engine
+
+class Traits(Base):
+    __tablename__ = 'traits'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    trait = Column(String, index=True)
+    score = Column(Integer, index=True)
+
+class TraitsQuestions(Base):
+    __tablename__ = 'traits_questions'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    question = Column(String, index=True)
+    trait_id = Column(UUID(as_uuid=True), ForeignKey("traits.id"))
+
+Base.metadata.create_all(engine)
