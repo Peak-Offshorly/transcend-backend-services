6541f8775866ca92b689757c16a29dd49ed4588e
Karlos <karlos@offshorly.com>
2024-04-10 14:03:08 +0800

    add: initial endpts for Post Save Initial Answers, Get Initial Questions, Get Traits, Post Save Chosen Strength and Weakness, Get Chosen Strength and Weakness, Get Followup Trait Questions, Post Save Followup Trait Answers, Get Trait Practice, Post Save Trait Practices

--- app/api/routes/initial_questions.py
+++ app/api/routes/initial_questions.py
@@ -12,6 +12,7 @@ from app.utils.initial_questions_crud import(
 db_dependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/initial-questions", tags=["initial-questions"])
 
+# Get Initial Questions
 @router.get("/all")
 async def get_all_initial_questions(db: db_dependency):
   try:
@@ -26,6 +27,7 @@ async def get_one_initial_question(id, db: db_dependency):
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
   
+# Post Save Initial Answers (would have calculations based on chosen answers)
 @router.post("/save-answers")
 async def save_initial_questions_answers(db: db_dependency):
   try:
--- app/api/routes/traits.py
+++ app/api/routes/traits.py
@@ -12,13 +12,23 @@ from app.utils.traits_crud import(
 db_dependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/traits", tags=["traits"])
 
+# Get Traits (Strengths and Weaknesses with the Scores)
 @router.get("/all")
 async def get_all_traits(db: db_dependency):
   try:
     return traits_get_all(db)
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
+  
+# Post Save Chosen Strength and Weakness
+@router.post("/save-chosen")
+async def save_traits_chosen(db: db_dependency):
+  try:
+    return None
+  except Exception as error:
+    raise HTTPException(status_code=400, detail=str(error))
 
+# Get Chosen Strength and Weakness
 @router.get("/{id}")
 async def get_one_trait(id, db: db_dependency):
   try:
@@ -26,8 +36,33 @@ async def get_one_trait(id, db: db_dependency):
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
   
-@router.post("/save-answers")
+# Get Followup Trait Questions (Should work the same for both strength and weakness); id=id of trait or user(?)
+@router.get("/get-questions/{id}")
+async def get_trait_questions(db: db_dependency):
+  try:
+    return None
+  except Exception as error:
+    raise HTTPException(status_code=400, detail=str(error))
+  
+# Post Save Followup Trait Answers; would have calculations based on answers to determine which practices to recommend; id=id of trait(?)
+@router.post("/save-answers/{id}")
 async def save_traits_answers(db: db_dependency):
+  try:
+    return None
+  except Exception as error:
+    raise HTTPException(status_code=400, detail=str(error))
+
+# Get Trait Practice; 
+@router.get("/get-practices/{id}")
+async def get_trait_practices(id, db: db_dependency):
+  try:
+    return None
+  except Exception as error:
+    raise HTTPException(status_code=400, detail=str(error))
+
+# Post Save Trait Practices 
+@router.post("/save-practices-answers/{id}")
+async def save_trait_practices_answers(id, db: db_dependency):
   try:
     return None
   except Exception as error:
