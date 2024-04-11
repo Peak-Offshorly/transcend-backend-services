8d8416f05257b995ac1b4c7422982bf88ff9fc64
Karlos <karlos@offshorly.com>
2024-04-11 13:41:41 +0800

    update: db models for Users, add: db models for DevPlan, Forms, UserTraits, Questions

--- app/database/models.py
+++ app/database/models.py
@@ -1,23 +1,10 @@
 import uuid
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime 
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
 from app.database.connection import Base, engine
 
-class Traits(Base):
-    __tablename__ = 'traits'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    trait = Column(String, index=True)
-    score = Column(Integer, index=True)
-
-class InitialQuestions(Base):
-    __tablename__ = 'initial_questions'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    question = Column(String, index=True)
-    trait_id = Column(UUID(as_uuid=True), ForeignKey("traits.id"))
-
-class Accounts(Base):
+class Users(Base):
     __tablename__ = 'users'
 
     id = Column(String, primary_key=True, index=True)
@@ -25,5 +12,44 @@ class Accounts(Base):
     password = Column(String, index=True)
     first_name = Column(String, index=True)
     last_name = Column(String, index=True)
+    role = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), index=True)
+    form_id = Column(UUID(as_uuid=True), ForeignKey("forms.id"))
+
+class DevelopmentPlan(Base):
+    __tablename__ = 'development_plan'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    user_id = Column(String, ForeignKey("users.id"))
+
+class Forms(Base):
+    __tablename__ = 'forms'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = Column(String, index=True)
+    user_id = Column(String, ForeignKey("users.id"))
+
+class UserTraits(Base):
+    __tablename__ = 'user_traits'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    user_id = Column(String, ForeignKey("users.id"))
+    traits_id = Column(UUID(as_uuid=True), ForeignKey("traits.id"))
+    score = Column(Integer, index=True)
+
+class Questions(Base):
+    __tablename__ = 'questions'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = Column(String, index=True)
+    forms_id = Column(UUID(as_uuid=True), ForeignKey("forms.id"))
+    option_type = Column(String, index=True)
+
+class Traits(Base):
+    __tablename__ = 'traits'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    trait = Column(String, index=True)
+    score = Column(Integer, index=True)
 
 Base.metadata.create_all(engine)
