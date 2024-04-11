50d3d43567b82faf827666b156f6b0647cc52488
Karlos <karlos@offshorly.com>
2024-04-11 11:05:24 +0800

    revert: database models changes

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,6 @@
 import uuid
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
-
 from app.database.connection import Base, engine
 
 class Traits(Base):
@@ -10,7 +8,7 @@ class Traits(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     trait = Column(String, index=True)
-    questions = relationship("InitialQuestions", backref="traits")
+    score = Column(Integer, index=True)
 
 class InitialQuestions(Base):
     __tablename__ = 'initial_questions'
@@ -19,14 +17,7 @@ class InitialQuestions(Base):
     question = Column(String, index=True)
     trait_id = Column(UUID(as_uuid=True), ForeignKey("traits.id"))
 
-class InitialQuestionsChoices(Base):
-    __tablename__ = 'initial_questions_choices'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    question_id = Column(UUID(as_uuid=True), ForeignKey("initial_questions.id"))
-    choice = Column(String, index=True)
-
-class Users(Base):
+class Accounts(Base):
     __tablename__ = 'users'
 
     id = Column(String, primary_key=True, index=True)
@@ -34,15 +25,5 @@ class Users(Base):
     password = Column(String, index=True)
     first_name = Column(String, index=True)
     last_name = Column(String, index=True)
-    initial_questions_response = relationship("UserInitialQuestionsResponse", backref="users")
-
-
-class UserInitialQuestionsResponse(Base):
-    __tablename__ = 'users_initial_questions_response'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
-    question_id = Column(UUID(as_uuid=True), ForeignKey("initial_questions.id"))
-    choice_id = Column(UUID(as_uuid=True), ForeignKey("initial_questions_choices.id"))
 
 Base.metadata.create_all(engine)
