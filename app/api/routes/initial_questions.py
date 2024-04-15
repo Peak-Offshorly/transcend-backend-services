958a1fa2ab0a5ebcdea470154180376c268180a8
ridiculoys <lltalip@up.edu.ph>
2024-04-15 19:22:34 +0800

    fix: comment out initial questions crud

--- app/api/routes/initial_questions.py
+++ app/api/routes/initial_questions.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import Session
 from typing import Annotated
 from app.firebase.session import firebase, auth
 from app.database.connection import get_db
-from app.utils.initial_questions_crud import(
-    initial_questions_get_all,
-    initial_questions_get_one
-)
+# from app.utils.initial_questions_crud import(
+#     initial_questions_get_all,
+#     initial_questions_get_one
+# )
 
 db_dependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/initial-questions", tags=["initial-questions"])
@@ -16,14 +16,16 @@ router = APIRouter(prefix="/initial-questions", tags=["initial-questions"])
 @router.get("/all")
 async def get_all_initial_questions(db: db_dependency):
   try:
-    return initial_questions_get_all(db)
+    # return initial_questions_get_all(db)
+    return {"message": "get all"}
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
 
 @router.get("/{initial_question_id}")
 async def get_one_initial_question(initial_question_id, db: db_dependency):
   try:
-    return initial_questions_get_one(db=db, id=id)
+    # return initial_questions_get_one(db=db, id=id)
+    return {"message": "get one"}
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
   
