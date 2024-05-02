5ff445d49cb98d8d659cd47b7d0c95d5a19402fa
Karlos <karlos@offshorly.com>
2024-05-02 12:02:50 +0800

    Merge branch 'feat/inactive-user'

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print("Started App:", os.environ.get("APP_NAME", "Peak Test App"))
 app: fastapi.FastAPI = create_app()
 
 async def check_user_activity():
-    async with AsyncClient(app=app, base_url="http://localhost:9001") as client:
+    async with AsyncClient(app=app, base_url="http://0.0.0.0:10000") as client:
         await client.get("/accounts/check-if-active")
 
 # Create an instance of the scheduler
@@ -28,11 +28,11 @@ if __name__ == "__main__":
     app="main:app",
     
     # Server Host and Port used for Render 
-    # host=os.environ.get("SERVER_HOST", "0.0.0.0"),
-    # port=os.environ.get("SERVER_PORT", 10000),
+    host=os.environ.get("SERVER_HOST", "0.0.0.0"),
+    port=os.environ.get("SERVER_PORT", 10000),
 
-    host=os.environ.get("SERVER_HOST", "127.0.0.1"),
-    port=os.environ.get("SERVER_PORT", 9001),
+    # host=os.environ.get("SERVER_HOST", "127.0.0.1"),
+    # port=os.environ.get("SERVER_PORT", 9001),
     log_level="info",
     reload=True,
   )
