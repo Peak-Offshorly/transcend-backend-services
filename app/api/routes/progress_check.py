8f6182f4fe7ed6905f9ed9602bb4569ce450a345
Karlos <karlos@offshorly.com>
2024-05-17 13:49:57 +0800

    add: save and get progress check answers

--- app/api/routes/progress_check.py
+++ app/api/routes/progress_check.py
@@ -4,10 +4,10 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
 from typing import Annotated
-from app.schemas.models import DataFormSchema
+from app.schemas.models import DataFormSchema, FormAnswerSchema
 from app.database.connection import get_db
 from app.utils.sprints_crud import sprint_create_get_one
-from app.utils.answers_crud import answers_get_all
+from app.utils.answers_crud import answers_get_all, answers_save_one
 from app.utils.forms_crud import form_questions_options_get_all, forms_with_questions_options_sprint_id_get_all, forms_create_one
 
 db_dependency = Annotated[Session, Depends(get_db)]
@@ -133,10 +133,38 @@ async def get_development_progress_questions_weakness_practice(db: db_dependency
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
 
-# Post Save Answers for Written Development Actions as Questions (Strength, Weakness)
-@router.post("/save-answers/{user_id}")
-async def save_development_progress_answers():
+# Post Save Answers for Written Development Actions as Questions - can be used for both Strength and Weakness
+@router.post("/save-answers")
+async def save_development_progress_answers(db: db_dependency, answers: FormAnswerSchema):
+  form_id = answers.form_id
+  form_name = answers.form_name
+  form_name_parts = form_name.split("_")
+
   try:
-    return { "message": "Development Actions Saved" }
+    for answer in answers.answers:
+      # Add all answers in DB 
+      await answers_save_one(
+        db=db,
+        form_id=form_id,
+        question_id=answer.question_id,
+        option_id=answer.option_id,
+        answer=answer.answer
+      )
+
+    return { "message": f"{form_name_parts[2].capitalize()} Development Actions Progress Check for Week {form_name_parts[-1]} Saved." }
+  except Exception as error:
+    raise HTTPException(status_code=400, detail=str(error))
+
+# Get answers for Progress Check - might be used for weekly email nudge to user
+@router.get("/get-answers")
+async def save_development_progress_answers(db: db_dependency, trait_type: str, sprint_number: int, week_number: int, user_id: str):
+  try:
+    if week_number > 6:
+      return { "message": "Week number too big." }
+  
+    # <SPRINT_NUM>_PROGRESS_WEAKNESS_WEEK_<WEEK NUMBER>
+    form_name = f"{sprint_number}_PROGRESS_{trait_type.upper()}_WEEK_{week_number}"
+
+    return await answers_get_all(db=db, user_id=user_id, form_name=form_name, sprint_number=sprint_number)
   except Exception as error:
     raise HTTPException(status_code=400, detail=str(error))
