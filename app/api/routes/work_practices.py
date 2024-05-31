e3ea95c3e063c51be8fe5210f66586eacde98297
Karlos <karlos@offshorly.com>
2024-05-31 09:46:13 +0800

    add: auth to work_practices endpts

--- app/api/routes/work_practices.py
+++ app/api/routes/work_practices.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import Annotated
 from app.database.connection import get_db
+from app.firebase.utils import verify_token
 from app.utils.dev_plan_crud import dev_plan_create_get_one
 from app.utils.forms_crud import forms_with_questions_options_get_all
 from app.utils.answers_crud import answers_save_one, answers_get_all, answers_clear_all
@@ -13,7 +14,14 @@ router = APIRouter(prefix="/work-practices", tags=["work-practices"])
 
 # Get Development Actions Form/Get Practice Guide Questions
 @router.get("/get-actions-form")
-async def get_development_actions_form(user_id: str, form_name: str, db: db_dependency):
+async def get_development_actions_form(user_id: str, form_name: str, db: db_dependency, token = Depends(verify_token)):
+  
+  if token != user_id:
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="You are not authorized to perform this action."
+    )
+  
   try:
     dev_plan = await dev_plan_create_get_one(user_id=user_id, db=db)
     return forms_with_questions_options_get_all(db, name=form_name, user_id=user_id, dev_plan_id=dev_plan["dev_plan_id"])
@@ -22,7 +30,13 @@ async def get_development_actions_form(user_id: str, form_name: str, db: db_depe
   
 # Post Save Development Actions Answers(Direct user input, at most 3)
 @router.post("/save-development-actions")
-async def save_development_actions(answers: FormAnswerSchema, db: db_dependency):
+async def save_development_actions(answers: FormAnswerSchema, db: db_dependency, token = Depends(verify_token)):
+  user_id = answers.user_id
+  if token != user_id:
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="You are not authorized to perform this action."
+    )
   form_id = answers.form_id
 
   try:
@@ -45,7 +59,12 @@ async def save_development_actions(answers: FormAnswerSchema, db: db_dependency)
 
 # Get Written Development Actions
 @router.get("/development-actions")
-async def get_development_actions(user_id: str, trait_type: str, sprint_number: int, db: db_dependency):
+async def get_development_actions(user_id: str, trait_type: str, sprint_number: int, db: db_dependency, token = Depends(verify_token)):
+  if token != user_id:
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="You are not authorized to perform this action."
+    )
   # Fetch from the Answers DB table under the form of strength/weakness practice
   form_name = f"{sprint_number}_{trait_type}_PRACTICE_QUESTIONS"
 
