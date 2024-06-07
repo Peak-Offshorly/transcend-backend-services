124c878926d4625b0d492fbc7f74d8d9e0dd5d1c
ridiculoys <lltalip@up.edu.ph>
2024-06-07 17:09:00 +0800

    add: conts file in ai folder

---
+++ app/ai/const.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ANTHROPIC_API_KEY=os.environ["ANTHROPIC_API_KEY"]
+OPENAI_API_KEY=os.environ["OPENAI_API_KEY"]
+PINECONE_API_KEY=os.environ["PINECONE_API_KEY"]
+USE_SERVERLESS=os.environ["USE_SERVERLESS"]
