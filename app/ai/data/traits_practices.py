e2b9b33275f544d8d8650f9e19cde143825a01ae
ridiculoys <lltalip@up.edu.ph>
2024-06-14 11:22:59 +0800

    add: pracctices formatter

--- app/ai/data/traits_practices.py
+++ app/ai/data/traits_practices.py
@@ -3,12 +3,17 @@ def get_chosen_traits(data):
   chosen_weakness = data["chosen_weakness"]["name"]
   return chosen_strength, chosen_weakness
 
-def get_top_traits(data):
-  top_strengths = [trait["name"] for trait in data["strengths"]]
-  top_weaknesses = [trait["name"] for trait in data["weaknesses"]]
-  return top_strengths, top_weaknesses
+def get_ten_traits(data):
+  strengths = [trait["name"] for trait in data["strengths"]]
+  weaknesses = [trait["name"] for trait in data["weaknesses"]]
+  return strengths, weaknesses
 
 def get_chosen_practices(data):
-  chosen_strength_practice = data["practices_sprint_1"]["strength_practice"]
-  chosen_weakness_practice = data["practices_sprint_1"]["weakness_practice"]
-  return chosen_strength_practice, chosen_weakness_practice
+  strength_practice = None
+  weakness_practice = None
+  if len(data['chosen_strength_practice']) > 0:
+    strength_practice = data['chosen_strength_practice'][0].name
+  if len(data['chosen_weakness_practice']) > 0:
+    weakness_practice = data['chosen_weakness_practice'][0].name
+    
+  return strength_practice, weakness_practice
