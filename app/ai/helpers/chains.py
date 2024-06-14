e961004de76ec1299b464c5ba07597d4c811489b
ridiculoys <lltalip@up.edu.ph>
2024-06-14 15:56:02 +0800

    fix: answer chain to be used for either strength or weakness

--- app/ai/helpers/chains.py
+++ app/ai/helpers/chains.py
@@ -9,7 +9,7 @@ from app.ai.helpers.tokens import count_tokens
 # GPT_MODEL = "gpt-3.5-turbo-0125"
 GPT_MODEL = "gpt-4o-2024-05-13"
 
-def generate_actions(docs, initial_questions_with_answers, strengths, weaknesses, chosen_strength, chosen_weakness, strength_practice, weakness_practice, company_size, industry, employee_role, role_description):
+def generate_actions(trait_type, docs, initial_questions_with_answers, five_traits, chosen_trait, trait_practice, company_size, industry, employee_role, role_description):
   print("Generating answer")
 
   prompt = PromptTemplate(
@@ -18,54 +18,59 @@ def generate_actions(docs, initial_questions_with_answers, strengths, weaknesses
     <|start_header_id|>system<|end_header_id|>
     You are an expert at creating detailed step-by-step actionable improvement points for a Leadership Development Plan. 
 
-    The employee will give you their personal and company details, their strengths and weaknesses as a leader, and the practice they want to focus on for each chosen strength and weakness.
+    The employee will give you their personal and company details, their answers to a leadership questionnaire, their {type} as a leader, and the practice they want to focus on for their chosen {type}.
     
-    Your task is to provide 10 detailed development actions that are feasible, measurable, and time-bound in order to guide the employee in leveraging their strengths and addressing their weaknesses. Provide exactly 5 actions for the chosen strength and exactly 5 actions for the chosen weakness.
-    The details for each action must ALWAYS include the following (2-5 sentences):
-    - Address the chosen strength and weakness, and the respective practices that the employee wants to focus on
-    - Be as thorough and specific in terms of what the employee should do.
-    - Include a clear frequency or schedule (e.g., daily, weekly).
-    - Be clear on the materials, tools, or resources required.
-    - Provide a step-by-step improvement strategy that is comprehensive and insightful.
-    - Be practical and reasonable given the employee's role and the company's context.
-    - Be measurable in terms of progress and impact on the employee's leadership skills.
+    Your task is to provide 5 detailed personal development actions that are feasible, measurable, and time-bound in order to guide the employee in leveraging and improving their {type}. Provide exactly 5 actions for the chosen {type}.
 
+    Provide your answer in a JSON format with "actions" as the key. It should contain a list of 5 actions as values. Each action in the list should be an object that contains the name, details, importance, and measurement.
+    The name pertains to the name or title of the action. 
+    The details contain the thorough details or information for the action as instructed below The details for each action, which should be 2-3 sentences, must ALWAYS include the following:
+    - Address the chosen {type} and the respective practice that the employee wants to focus on
+    - Be as thorough and specific in terms of what the employee should do
+    - Include a clear frequency or schedule (e.g., daily, weekly)
+    - Be clear on the materials, tools, or resources required
+    - Provide a step-by-step improvement strategy that is comprehensive and insightful
+    - Be practical and reasonable given the employee's role and the company's context
+    - Be measurable in terms of progress and impact on the employee's leadership skills
     
-    Provide your answer in a JSON format with "strength" and "weakness" as keys. Each should have a list of 5 actions as values. Each action in the list should be an object that contains the name, details, importance, and measurement. The name pertains to the name or title of the action, the details contain the thorough steps or instructions for the action, the importance is the significance of the action in developing the employee's leadership skills, and the measurement is how the action can be measured over time. Be as detailed and precise as possible in your response.
+    The importance is the significance of the action in developing the employee's leadership skills.
+    The measurement is how the action can be measured over time. Be as detailed and precise as possible in your response.
 
-    Use the following pieces of retrieved context to help GUIDE your answer. Your answer is not constrained to these pieces and you may use knowledge beyond the retrieved context, but take a deep breath and thoroughly make sure the details you generate are factual and reasonable.
+    Also use the following pieces of retrieved context to help GUIDE your answer. Your answer is not constrained to these pieces and you may use knowledge beyond the retrieved context, but take a deep breath and thoroughly make sure the details you generate are factual and reasonable.
     Context: {context}
 
     Make sure to understand the task you are given and provide a detailed, insightful, and correctly-formatted response that is relevant to the employee's details and needs.
     
     <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Use the given employee's details and answers to help finetune your answer. 
+
+    Leadership Questionnaire Answers:
+    {initial_questions}
+    
     Employee Details:
     Industry: {industry}
     Role: {employee_role}
     Role Description: {role_description}
     Company Size: {company_size} employees
 
-    It has been identified that their top 5 strengths are: {strengths} and their top 5 weaknesses are: {weaknesses}.
+    It has been identified that their top 5 {type}s are: {five_traits}.
 
-    They want to work on this strength: "{chosen_strength}"
-    Focused on their identified practice for that strength: "{strength_practice}"
-    They want to work on this weakness: "{chosen_weakness}"
-    Focused on their identified practice for that weakness: "{weakness_practice}"
+    They want to work on this {type}: "{chosen_trait}"
+    Focused on their identified practice for that {type}: "{trait_practice}"
 
     Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>
     """,
-    input_variables=["context", "strengths", "weaknesses", "chosen_strength", "chosen_weakness", "strength_practice", "weakness_practice", "company_size", "industry", "employee_role", "role_description"],
+    input_variables=["type", "context", "initial_questions", "five_traits", "chosen_trait", "trait_practice", "company_size", "industry", "employee_role", "role_description"],
     )
   
   data_to_tokenize = [
+    trait_type,
     prompt.template,
     docs,
-    strengths,
-    weaknesses,
-    chosen_strength,
-    chosen_weakness,
-    strength_practice,
-    weakness_practice,
+    initial_questions_with_answers,
+    five_traits,
+    chosen_trait,
+    trait_practice,
     company_size,
     industry,
     employee_role,
@@ -82,22 +87,21 @@ def generate_actions(docs, initial_questions_with_answers, strengths, weaknesses
   chain = prompt | llm | JsonOutputParser()
 
   inputs = {
+    "type": trait_type,
     "context": docs,
-    "strengths": strengths,
-    "weaknesses": weaknesses,
-    "chosen_strength": chosen_strength,
-    "chosen_weakness": chosen_weakness,
-    "strength_practice": strength_practice,
-    "weakness_practice": weakness_practice,
+    "initial_questions": initial_questions_with_answers,
+    "five_traits": five_traits,
+    "chosen_trait": chosen_trait,
+    "trait_practice": trait_practice,
     "company_size": company_size,
     "industry": industry,
     "employee_role": employee_role,
     "role_description": role_description
   }
 
-  generation = chain.invoke(inputs)
+  response = chain.invoke(inputs)
   # print(generation)
-  return generation
+  return response
 
 
 #add hallucination checker and formatting grader function
