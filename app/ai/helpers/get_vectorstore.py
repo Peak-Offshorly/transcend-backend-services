d1655f09a86a0982df44b0030831d46043b327e7
ridiculoys <lltalip@up.edu.ph>
2024-06-20 16:19:53 +0800

    chore: remove print

--- app/ai/helpers/get_vectorstore.py
+++ app/ai/helpers/get_vectorstore.py
@@ -4,7 +4,7 @@ from langchain_pinecone import PineconeVectorStore
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 def get_vectorstore(index_name: str, embedding_model: str = EMBEDDING_MODEL):
-  print("Retrieving Pinecone Vectorstore")
+  # print("Retrieving Pinecone Vectorstore")
   embeddings = OpenAIEmbeddings(model=embedding_model)
   pinecone_vectorstore = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embeddings)
   
