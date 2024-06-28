2ca221ce7147ef2a389f1621817eccab8d872279
ridiculoys <lltalip@up.edu.ph>
2024-06-28 11:39:06 +0800

    add: commened out way to tokenize

--- app/ai/helpers/tokens.py
+++ app/ai/helpers/tokens.py
@@ -7,3 +7,23 @@ def count_tokens(text, encoding_name):
   # encoding = tiktoken.encoding_for_model(encoding_name)
   tokens = len(encoding.encode(text))
   return tokens
+
+
+#test
+# data_to_tokenize = [
+  #   trait_type,
+#   prompt.template,
+#   docs,
+#   initial_questions_with_answers,
+#   five_traits,
+#   chosen_trait,
+#   trait_practice,
+#   company_size,
+#   industry,
+#   employee_role,
+#   role_description
+# ]
+
+# encoding_model = "o200k_base"
+# tokens_total = sum(count_tokens(element, encoding_model) for element in data_to_tokenize)
+# print(f"Tokens Used: {tokens_total}")
