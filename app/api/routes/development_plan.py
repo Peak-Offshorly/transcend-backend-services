f94a7b207d0f45537e67d0a9aa46f4416e751928
Karlos <karlos@offshorly.com>
2024-07-08 12:52:46 +0800

    fix last commit(3f6bd67); prod setup

--- app/api/routes/development_plan.py
+++ app/api/routes/development_plan.py
@@ -186,8 +186,9 @@ async def get_current_week(user_id: str, db: db_dependency, token = Depends(veri
     # # FOR DEV TESTING - increment weeks every 2 minutes
     # minutes_elapsed = delta.total_seconds() // 60  # Convert the timedelta to minutes
     # week_number = int((minutes_elapsed // 2) + 1)  # Increment week every 120 minutes (2 minutes * 60 seconds)
-    # if week_number > 12:
-    #   week_number = 12
+
+    if week_number > 12:
+      week_number = 12
 
     return{
       "week_number": week_number
