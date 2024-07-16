565edaca3541401853aa87e39d0dd8a0ca9eeef2
aalbayda <aalbayda@up.edu.ph>
2024-07-17 02:05:21 +0800

    init: peak chatbot

--- app/ai/const.py
+++ app/ai/const.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ANTHROPIC_API_KEY=os.environ["ANTHROPIC_API_KEY"]
-OPENAI_API_KEY=os.environ["OPENAI_API_KEY"]
-PINECONE_API_KEY=os.environ["PINECONE_API_KEY"]
-USE_SERVERLESS=os.environ["USE_SERVERLESS"]
+# ANTHROPIC_API_KEY=os.environ["ANTHROPIC_API_KEY"]
+# OPENAI_API_KEY=os.environ["OPENAI_API_KEY"]
+# PINECONE_API_KEY=os.environ["PINECONE_API_KEY"]
+# USE_SERVERLESS=os.environ["USE_SERVERLESS"]
